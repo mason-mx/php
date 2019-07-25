9d77de473c3d2bff522a95860665f485071c4a4a
Mason <star.wang@live.com>
2019-07-25 14:48:33 +1200

    a simple php server

---
+++ basic-php/includes/head-tag.php
@@ -0,0 +1,14 @@
+<title><?php print $PAGE_TITLE;?></title>
+
+<?php if ($CURRENT_PAGE == "Index") { ?>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" /> 
+<?php } ?>
+
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<style>
+	.footer {
+		font-size: 14px;
+		text-align: center;
+	}
+</style>
